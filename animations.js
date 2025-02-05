4ce4f5f35fd0f9aa93d3bf6cd701fccd6aa867d6
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2025-02-05 17:14:17 -0600

    modal finally works!
    The styling and coloring of the modals is a bit weird though. I can scroll in modal too! data loads from json now

--- animations.js
+++ animations.js
@@ -1,3 +1,4 @@
+
 // Sorting animations
 $(document).ready(function() {
   $(".button").click(function() {
@@ -41,34 +42,6 @@ $(document).ready(function() {
 // Modal data
 let mArray = [];
 //template: { id: "", src: "./static/loveanddeepspace/.png", modal: "modal80", label: "", data: ""},
-
-
-$.get( "./data.json", function( data){ 
-  mArray = data; 
-  console.log(mArray); 
-  // Inject images into the project grid
-  let grid = $("#project-grid-items");
-  mArray.forEach(item => {
-      grid.append(`
-          <div id="project-grid-item" class="box filter ${item.data} name col-lg-3 col-md-4 mb-12 mb-md-0">
-        <a href="#">
-          <img class="project-grid-item-img" src="${item.src}" alt="" id="${item.id}"/>
-        </a>
-        <div class="overlay">
-          <div class="overlay-text"><button class="modal-btn" href="#${item.modal}">${item.label}</button></div>
-        </div>
-      </div>
-      `);
-  });
-
-  // Show the photos section once loaded
-  $("#photos").fadeIn("1000");
-});
-
-
-let modalT = '';
-let modalL = '';
-
 function loadingLoader() {
   Loader = setTimeout(showPage, 3000);
 }
@@ -78,6 +51,39 @@ function showPage() {
   document.getElementById("mainContent").style.display = "block";
   document.getElementById("photos").style.display = "block";
 }
+
+$.get( "./data.json", function( data){ 
+  mArray = data; 
+  console.log(mArray); 
+  // Inject images into the project grid
+  let grid = $("#project-grid-items");
+  mArray.forEach(item => {
+    grid.append(`
+      <div id="project-grid-item" class="box filter ${item.data} name col-lg-3 col-md-4 mb-12 mb-md-0">
+        <a href="#">
+          <img class="project-grid-item-img" src="${item.src}" alt="" id="${item.id}"/>
+        </a>
+        <div class="overlay">
+          <div class="overlay-text">
+            <button class="modal-btn btn btn-primary" data-target="#${item.modal}">${item.label}</button>
+          </div>
+        </div>
+      </div>
+    `);
+    
+  });
+
+  // Show the photos section once loaded
+  $("#photos").fadeIn("1000");
+
+  generateModals();
+});
+
+
+let modalT = '';
+let modalL = '';
+
+
 
 // Generate grid items and modals
 /* function generateGridItems() {
@@ -98,47 +104,42 @@ function showPage() {
 } */
 
   function generateModals() {
+    console.log("Generating modals..."); // Debugging
     mArray.forEach(item => {
       const modal = document.createElement('div');
-      modal.classList.add('modal');
+      modal.classList.add('modal', 'fade');
       modal.id = item.modal;
+      modal.setAttribute('tabindex', '-1');
+      modal.setAttribute('role', 'dialog');
   
-      const modalContent = document.createElement('div');
-      modalContent.classList.add('modal-content');
+      modal.innerHTML = `
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">${item.label}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img src="${item.src}" class="img-fluid"/>
+            </div>
+          </div>
+        </div>
+      `;
   
-      const modalBody = document.createElement('div');
-      modalBody.classList.add('modal-body');
-  
-      const closeButton = document.createElement('span');
-      closeButton.classList.add('close');
-      closeButton.innerHTML = '&times;';
-      closeButton.onclick = () => {
-        modal.style.display = 'none';
-      };
-  
-      const figure = document.createElement('figure');
-      figure.classList.add('mbc_container');
-  
-      const image = document.createElement('img');
-      image.src = item.src;
-  
-      figure.appendChild(image);
-      modalBody.appendChild(closeButton);
-      modalBody.appendChild(figure);
-      modalContent.appendChild(modalBody);
-      modal.appendChild(modalContent);
-  
-      document.body.appendChild(modal); 
+      document.body.appendChild(modal);
     });
   }
+  
 
 loadingLoader();
 //generateGridItems();
-generateModals();
+
 
 // Modal functions
 var modals = document.querySelectorAll(".modal");
-var modalbtn = document.querySelectorAll("button.modal-btn");
+//var modalbtn = document.querySelectorAll("button.modal-btn");
 var spans = document.getElementsByClassName("close");
 
 for (var i = 0; i < spans.length; i++) {
@@ -149,13 +150,15 @@ for (var i = 0; i < spans.length; i++) {
   }
 }
 
-for (var i = 0; i < modalbtn.length; i++) {
-  modalbtn[i].onclick = function (e) {
-    e.preventDefault();
-    var modal = document.querySelector(e.target.getAttribute("href"));
-    modal.style.display = "block";
-  };
-}
+$(document).on("click", ".modal-btn", function (e) {
+  e.preventDefault();
+  var targetModal = $(this).data("target");
+  console.log("Opening modal:", targetModal); // Debugging
+  console.log("Modal button clicked, target:", targetModal);
+  console.log("Checking if modal exists:", $(targetModal));
+  $(targetModal).modal("handleUpdate");
+  $(targetModal).modal("show");
+});
 
 window.onclick = function(event) {
   if (event.target.classList.contains("modal")) {
